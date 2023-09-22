3eb1cf4639165b682e70c1dac37f40bd211d6bef
Otavio Gonzaga <otaviolgonzaga@gmail.com>
2023-09-22 20:23:35 -0300

    redirect fix

--- script.js
+++ script.js
@@ -3,18 +3,10 @@ const form = document.querySelector("form")
 
 form.onsubmit = (e) => {
 	e.preventDefault()
-	window.location.href = `https://wa.me/+55${num.value}`
+	window.location.replace(`https://wa.me/+55${num.value}`)
 }
 num.focus()
 
 num.addEventListener("input", (e) => {
 	console.log(e.target.value)
 })
-
-num.addEventListener("keypress", (e) => {
-	if (e.code === "Enter") Submit()
-})
-
-function Submit() {
-	console.log("aa")
-}
